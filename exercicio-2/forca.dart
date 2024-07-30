e35011e353708c6ebbd22090ec8e83e12370d6fe
lauravivan <lauvivang@gmail.com>
2024-07-30 16:46:31 -0300

    Build game

--- exercicio-2/forca.dart
+++ exercicio-2/forca.dart
@@ -1,21 +1,90 @@
-void main() {
-  //JOgo da forca
+import 'package:word_generator/word_generator.dart';
+import 'dart:io';
 
-  // TO do: ter uma lista de palavras aleatórias e ir randomicamente sorteando.
-  /**
-   * 
-   * exemplo: cavalo
-   * 
-   * chances: 5
-   * 
-   * letra: A
-   * 
-   * faltam: c, v, l, o
-   * 
-   * letra: B
-   * 
-   * errado, chances: 4
-   * 
-   * letra: 
-   */
+String palavraSorteada = getPalavraSorteada();
+final QNT_DE_CHANCES_TOTAIS = 15;
+int qtdDeChancesRestantes = 15;
+String mascaraPalavraSorteada = '_' * palavraSorteada.length;
+String letraEscolhida = '';
+
+void main() {
+  print('----- 👽 JOGO DA FORCA 👽 -----');
+  print('Desenvolvido por Laura Vivan Gonçalves');
+  print('');
+
+  iniciarJogo();
+}
+
+String getPalavraSorteada() {
+  final wordGenerator = WordGenerator();
+  String noun = wordGenerator.randomNoun();
+  return noun;
+}
+
+void updateMascaraPalavraSorteada() {
+  List<int> indexesDasLetrasAcertadas = getIndexesDasLetrasAcertadas();
+
+  for (int i = 0; i < indexesDasLetrasAcertadas.length; i++) {
+    mascaraPalavraSorteada = mascaraPalavraSorteada.replaceRange(
+        indexesDasLetrasAcertadas[i],
+        indexesDasLetrasAcertadas[i] + 1,
+        letraEscolhida);
+  }
+}
+
+List<int> getIndexesDasLetrasAcertadas() {
+  List<int> indexes = [];
+  int indexRef = palavraSorteada.indexOf(letraEscolhida);
+
+  while (indexRef != -1) {
+    indexes.add(indexRef);
+    indexRef = palavraSorteada.indexOf(letraEscolhida, indexRef + 1);
+  }
+
+  return indexes;
+}
+
+void setLetra() {
+  stdout.write('Escolha uma letra: ');
+  String usuarioInput = stdin.readLineSync()!;
+
+  String letra = usuarioInput.substring(0);
+
+  letraEscolhida = letra.toLowerCase();
+}
+
+bool isLetraNaPalavra() {
+  return palavraSorteada.contains(letraEscolhida);
+}
+
+bool isJogoEmAndamento() {
+  return mascaraPalavraSorteada.contains('_');
+}
+
+void iniciarJogo() {
+  while (isJogoEmAndamento()) {
+    setLetra();
+    bool letraEstaNaPalavra = isLetraNaPalavra();
+
+    if (letraEstaNaPalavra) {
+      updateMascaraPalavraSorteada();
+      print(mascaraPalavraSorteada);
+    } else {
+      qtdDeChancesRestantes--;
+      print('Você tem $qtdDeChancesRestantes chances');
+      print('❌' * (QNT_DE_CHANCES_TOTAIS - qtdDeChancesRestantes));
+      print('✅' * qtdDeChancesRestantes);
+    }
+  }
+
+  print('WIIII VOCÊ VENCEU O JOGO DA FORCA!! DESEJA JOGAR NOVAMENTE? (s/n)');
+  String continuar = stdin.readLineSync()!;
+
+  if (continuar.trim() == 's') {
+    palavraSorteada = getPalavraSorteada();
+    qtdDeChancesRestantes = 15;
+    mascaraPalavraSorteada = '_' * palavraSorteada.length;
+    letraEscolhida = '';
+    iniciarJogo();
+  }
 }
